31ae47d944acf3fc67e552b547f3c035a0333c31
klaferr <32842917+klaferr@users.noreply.github.com>
2023-09-13 15:51:22 -0400

    Update smooth_parallel_test1.sh

--- Data/Kris/shfiles/smooth_parallel_test1.sh
+++ Data/Kris/shfiles/smooth_parallel_test1.sh
@@ -1,14 +1,18 @@
-!/usr/bin/
+#!/usr/bin/bash
 
 # SBATCH -A bramsona
-
+# SBATCH -t 0:24:00:00
+# SBATCH --mem-per-cpu = 5G
+# SBATCH -e slurm-%j.err
+# SBATCH -o slurm-%j.out
 
 python create_smooth.py -molecule "H2O" -noon 0 -num 10000 -dirc "/users/klaferri/Desktop/Lunar_diurnal_migration/Results"
 
 
 for i in $(seq 10)
 do
-   python smooth_parallel_run.py -molecule "H2O" -noon 0 -time 24 -dt 0.25 -num 10000 -segment $i -dirc "/users/klaferri/DEsktop/Lunar_diurnal_migration/Results"
+   srun --cores=3 --mem=5g python smooth_parallel_run.py
+   #python smooth_parallel_run.py -molecule "H2O" -noon 0 -time 24 -dt 0.25 -num 10000 -segment $i -dirc "/users/klaferri/DEsktop/Lunar_diurnal_migration/Results"
 done
 
 
